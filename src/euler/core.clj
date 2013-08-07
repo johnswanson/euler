3a8e9d9e55d90c147621fe48c73447df89fd1812
John Swanson <john.d.swanson@gmail.com>
2013-08-07 14:15:53 -0700

    problem 5

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,4 +1,15 @@
 (ns euler.core
+  (:require [clojure.math.numeric-tower :as math])
   (:require [euler.utils :as utils]))
 
 (utils/set-prime-cache! 1000)
+
+(defn smallest-multiple [n]
+  (->> (range 2 (inc n))
+    (map (comp frequencies utils/factor))
+    (reduce #(merge-with max %1 %2))
+    (map (fn [[k v]] (math/expt k v)))
+    (reduce *)))
+
+(defn combine [current add]
+  (merge-with max current add))
--- src/euler/utils.clj
+++ src/euler/utils.clj
@@ -3,9 +3,18 @@
 
 (declare sieve prime?)
 
-(def prime-cache (atom {}))
-
 (defn divisible? [n divisor] (= 0 (mod n divisor)))
+
+(defn factor [n]
+  {:post [(every? prime? %)]}
+  (let [f1 (->> (range 2 (math/floor (min (inc (math/sqrt n))
+                                          (inc (/ n 2)))))
+             (filter #(divisible? n %))
+             (first))
+        f2 (if f1 (/ n f1))]
+    (if f1
+      (concat (factor f1) (factor f2))
+      [n])))
 
 (defn factors [n]
   (filter #(= 0 (mod n %)) (nnext (range (inc (math/round (/ n 2)))))))
@@ -16,25 +25,26 @@
 (defn only-odd [c]
   (filter odd? c))
 
-(defn set-prime-cache! [n]
-  (reset! prime-cache (apply hash-map (interleave (sieve n)
-                                                  (repeat true)))))
-
 (defn prime-factors [n]
   (filter prime? (only-odd (rev-order-factors n))))
 
 (defn largest-prime-factor [n]
   (first (prime-factors n)))
 
+(def prime-cache (atom {}))
+
+(defn set-prime-cache! [n]
+  (reset! prime-cache (apply hash-map (interleave (sieve n)
+                                                  (repeat true)))))
+
 (defn prime? [n]
-  (println "checking for primality:" n)
   (if (or (get @prime-cache n)
           (and (empty? (filter
                         #(< (apply max (keys @prime-cache)) %)
                         (factors n)))
                (not-any? (partial divisible? n) (keys @prime-cache))))
     (do
-      (swap! prime-cache assoc n true)
+      (if (not= 1 n) (swap! prime-cache assoc n true))
       true)
     false))
 
