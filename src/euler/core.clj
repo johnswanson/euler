476759e1ef826f8bdf62393c7562271db3449924
John Swanson <john.d.swanson@gmail.com>
2013-08-11 07:17:26 -0700

    problem 21 - amicable numbers

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -2,8 +2,35 @@
   (:require [clojure.math.numeric-tower :as math]
             [clojure.string :refer [split trim]]
             [euler.utils :as utils]
+            [clojure.set :refer [union]]
             [clojure.math.combinatorics :as combo :refer [permutations]]))
 
+(defn problem-21
+  "Amicable numbers
+  Problem 21
+  Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+  which divide evenly into n).
+  If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and
+  each of a and b are called amicable numbers.
+
+  For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44,
+  55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4,
+  71 and 142; so d(284) = 220.
+
+  Evaluate the sum of all the amicable numbers under 10000."
+  ([] (problem-21 10001))
+  ([n]
+   (let [cache (atom {})
+         amicable? (fn [sumfactors n]
+                     (let [a n
+                           b (sumfactors n)]
+                       (and (= a (sumfactors b))
+                            (not= a b))))
+         data (->> (range 2 n)
+                (map (fn [n] {n (reduce + (utils/proper-divisors n))}))
+                (apply merge))]
+     (->> (filter (partial amicable? data) (range 2 n))
+       (reduce +)))))
 
 (defn problem-9 []
   (for [a (range 1 1000)
