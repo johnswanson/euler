104ac26b23fb0fc8e71c50b4b516adc042edee21
John Swanson <john.d.swanson@gmail.com>
2013-08-12 09:49:37 -0700

    problem 22

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,7 +1,8 @@
 (ns euler.core
   (:require [clojure.math.numeric-tower :as math]
-            [clojure.string :refer [split trim]]
+            [clojure.string :refer [split trim lower-case] :as string]
             [euler.utils :as utils]
+            [clojure.java.io :as io]
             [clojure.set :refer [union]]
             [clojure.math.combinatorics :as combo :refer [permutations]]))
 
@@ -30,6 +31,30 @@
                 (map (fn [n] {n (reduce + (utils/proper-divisors n))}))
                 (apply merge))]
      (->> (filter (partial amicable? data) (range 2 n))
+       (reduce +)))))
+
+(defn problem-22
+  "
+  Using names.txt (right click and 'Save Link/Target As...'), a 46K text file
+  containing over five-thousand first names, begin by sorting it into
+  alphabetical order. Then working out the alphabetical value for each name,
+  multiply this value by its alphabetical position in the list to obtain a name
+  score.
+
+  For example, when the list is sorted into alphabetical order, COLIN, which is
+  worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
+  would obtain a score of 938 × 53 = 49714.
+
+  What is the total of all the name scores in the file?"
+  ([] (problem-22 "problem-22.txt"))
+  ([filename]
+   (letfn [(value-char [c] (- (int c) (dec (int \a))))
+           (value [s] (reduce + (map value-char (lower-case s))))
+           (score [i s] (* (inc i) (value s)))]
+     (->> (split (slurp (str (io/resource filename))) #",")
+       (map #(string/replace % #"\"" ""))
+       (sort)
+       (map-indexed score)
        (reduce +)))))
 
 (defn problem-9 []
