5680f725561da738bb2d29515780affb12634f93
John Swanson <john.d.swanson@gmail.com>
2013-08-14 14:26:49 -0700

    problem 28

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -114,6 +114,30 @@
       (filter sunday?)
       (count))))
 
+(defn problem-28
+  "Starting with the number 1 and moving to the right in a clockwise direction
+  a 5 by 5 spiral is formed as follows:
+
+  21 22 23 24 25
+  20  7  8  9 10
+  19  6  1  2 11
+  18  5  4  3 12
+  17 16 15 14 13
+
+  It can be verified that the sum of the numbers on the diagonals is 101.
+
+  What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral
+  formed in the same way?"
+  ([] (problem-28 1001))
+  ([sides]
+   (letfn [(diagonal [square]
+             (if (<= square 1)
+               '(1)
+               (let [corners (iterate #(- % (dec (math/sqrt square))) square)]
+                 (concat (take 4 corners)
+                         (diagonal (nth corners 4))))))]
+     (reduce + (diagonal (* sides sides))))))
+
 
 (defn problem-9 []
   (for [a (range 1 1000)
